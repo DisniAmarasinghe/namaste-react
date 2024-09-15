29e40b2f94f42ecfa32d1dce5d86e44e024289d2
DisniAmarasinghe <128348166+DisniAmarasinghe@users.noreply.github.com>
2024-09-15 20:09:56 +0530

    components

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Namaste react
+
+#Parcel
+-Dev Build
+-Local server
+-tree shaking
+-HMR
+-File watching algorithm - written in c#
+-Caching - faster builds
--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+*Header
+  -logo
+  -nav items
+*Body
+  -search
+  -restaurantContainer
+    -RestaurantCard
+*Footer
+  -copyright
+  -lnks
+  -address
+  -contact
+*/
+
+const Title = (
+  <h1 className="heading" tabIndex="5">
+    Hello React
+  </h1>
+);
+
+//React component
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <h1>React Functional Component </h1>;
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
 /*
 <div id="parent">
     <div id="child1">
@@ -11,25 +45,19 @@
 </div>
 */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-[  React.createElement(
-    "div",
-    { id: "child" },
-[    React.createElement("h1", {}, "Im h1 tag"),
-    React.createElement("h2", {}, "Im h2 tag")
-]  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
-[    React.createElement("h1", {}, "Im h1 tag"),
-    React.createElement("h2", {}, "Im h2 tag")
-]  )
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "Im h1 tag"),
+//     React.createElement("h2", {}, "Im h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "Im h1 tag"),
+//     React.createElement("h2", {}, "Im h2 tag"),
+//   ]),
+// ]);
 
-const heading = React.createElement("h1", { id: "heading" }, "hello react");
+// const heading = React.createElement("h1", { id: "heading" }, "hello react");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// root.render(parent);
